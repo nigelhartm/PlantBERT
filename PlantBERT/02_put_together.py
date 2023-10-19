8ec0135dc4c4f01b13ce5d4130e2e8b414d9ec7b
nigelhartm <nigel.hartman@outlook.de>
2023-10-19 20:39:26 +0200

    test

--- PlantBERT/02_put_together.py
+++ PlantBERT/02_put_together.py
@@ -2,16 +2,22 @@ import glob
 import sys
 import random
 
-for folder in ['plants', 'other']:
-        in_files = []
-        out_file = open('/usr/users/nigel.hartman/data/' + folder + '/all_sample_' + folder + '.fna', 'w')
-        for file in glob.glob('/usr/users/nigel.hartman/data/' + folder + '/sample_*.fna'):
-                in_files.append(open(file, 'r'))
-        while len(in_files) >= 1:
-                file = random.randint(0, len(in_files)-1)
-                line = in_files[file].readline()
-                if line:
-                        out_file.write(line)
-                else:
-                        print('File finished')
-                        in_files.pop(file)
+folder = ""
+
+if len(sys.argv) >= 2:
+        folder = str(sys.argv[1])
+if folder != 'plants' and folder !=  'other':
+	sys.exit("ERROR: use parameter plants or other")
+
+in_files = []
+out_file = open('/usr/users/nigel.hartman/data/' + folder + '/all_sample_' + folder + '.fna', 'w')
+for file in glob.glob('/usr/users/nigel.hartman/data/' + folder + '/sample_*.fna'):
+	in_files.append(open(file, 'r'))
+while len(in_files) >= 1:
+	file = random.randint(0, len(in_files)-1)
+	line = in_files[file].readline()
+	if line:
+		out_file.write(line)
+	else:
+		print('File finished')
+		in_files.pop(file)
--- PlantBERT/run.sh
+++ PlantBERT/run.sh
@@ -1,8 +1,15 @@
-echo "STEP: 00_download_genomes.sh"
-bash 00_download_genomes.sh
-echo "STEP: 02_put_together.py"
-python 02_put_together.py
-echo "STEP: 03_tokenizer_bpe.py"
-python 03_tokenizer_bpe.py
-echo "NOT Done because harder to debug STEP: 04_tokenize_data.py"
-#python 04_tokenize_data.py plants
+#!/bin/bash
+
+if [ "$1" = "plants" ] || [ "$1" = "other" ]
+then
+	echo "STEP: 00_download_genomes.sh"
+	bash 00_download_genomes.sh $1
+	echo "STEP: 02_put_together.py"
+	python 02_put_together.py $1
+	echo "STEP: 03_tokenizer_bpe.py"
+	python 03_tokenizer_bpe.py $1
+	echo "STEP: 04_tokenize_data.py"
+	python 04_tokenize_data_map_dataset.py $1
+else
+	echo "ERROR: parameter not existing (eg. plants or other)"
+fi
